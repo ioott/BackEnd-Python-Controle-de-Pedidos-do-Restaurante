64fdd81417402c3dd405578d633525c1828bbc2f
ioott <vioott@gmail.com>
2023-03-13 21:51:37 -0300

    feat: cria um sistema que mantenha um registro contínuo das informações contidas nos pedidos
    O sistema permite que, a qualquer momento, seja feita a extração das seguintes informações:
    
    Prato favorito por cliente;
    Pratos nunca pedidos por cada cliente;
    Dias nunca visitados por cada cliente;
    Dia mais movimentado;
    Dia menos movimentado.

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -10,47 +10,47 @@ def analyze_log(path_to_file):
                 open('data/mkt_campaign.txt', 'w') as output_file:
             csv_read = csv.reader(csvfile)
 
-            data = list(csv_read)
-            possible_days, possible_items = possible_days_and_items(data)
-            joao_orders, joao_days = about_joao(data)
-            maria_most_ordered = about_maria(data)
-            arnaldo_hamburguer_count = about_arnaldo(data)
+            orders_data = list(csv_read)
+            possible_days, possible_items = days_and_items(orders_data)
+            customer_orders, days_by_customer = customer_info(orders_data)
+            items_most_ordered = items_most_ordered_function(orders_data)
+            items_count = items_count_function(orders_data)
 
-            output_file.write(f"{maria_most_ordered}\n")
-            output_file.write(f"{arnaldo_hamburguer_count}\n")
-            output_file.write(f"{possible_items - joao_orders}\n")
-            output_file.write(f"{possible_days - joao_days}\n")
+            output_file.write(f"{items_most_ordered}\n")
+            output_file.write(f"{items_count}\n")
+            output_file.write(f"{possible_items - customer_orders}\n")
+            output_file.write(f"{possible_days - days_by_customer}\n")
 
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
 
 
-def possible_days_and_items(data):
-    possible_items = set(row[1] for row in data)
-    possible_days = set(row[2] for row in data)
+def days_and_items(orders_data):
+    possible_items = set(row[1] for row in orders_data)
+    possible_days = set(row[2] for row in orders_data)
     return [possible_days, possible_items]
 
 
-def about_maria(data):
-    maria_orders = {}
-    for row in data:
+def items_most_ordered_function(orders_data):
+    customer_orders = {}
+    for row in orders_data:
         client, order = row[0], row[1]
         if client == 'maria':
-            maria_orders[order] = maria_orders.get(order, 0) + 1
-    maria_most_ordered = max(maria_orders, key=maria_orders.get)
-    return maria_most_ordered
+            customer_orders[order] = customer_orders.get(order, 0) + 1
+    items_most_ordered = max(customer_orders, key=customer_orders.get)
+    return items_most_ordered
 
 
-def about_arnaldo(data):
-    arnaldo_hamburguer_count = sum(
-        1 for row in data
+def items_count_function(orders_data):
+    items_count = sum(
+        1 for row in orders_data
         if row[0] == 'arnaldo'
         and row[1] == 'hamburguer'
     )
-    return arnaldo_hamburguer_count
+    return items_count
 
 
-def about_joao(data):
-    joao_orders = set(row[1] for row in data if row[0] == 'joao')
-    joao_days = set(row[2] for row in data if row[0] == 'joao')
-    return [joao_orders, joao_days]
+def customer_info(orders_data):
+    customer_orders = set(row[1] for row in orders_data if row[0] == 'joao')
+    days_by_customer = set(row[2] for row in orders_data if row[0] == 'joao')
+    return [customer_orders, days_by_customer]
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,66 @@
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self._data = []
+
     def __len__(self):
-        pass
+        return len(self._data)
 
     def add_new_order(self, customer, order, day):
-        pass
+        orders_data = self._data
+        new_order = (customer, order, day)
+        orders_data.append(new_order)
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        orders_data = self._data
+        customer_orders = {}
+
+        for row in orders_data:
+            client, order = row[0], row[1]
+            if client == customer:
+                customer_orders[order] = customer_orders.get(order, 0) + 1
+
+        return max(customer_orders, key=customer_orders.get)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        orders_data = self._data
+        possible_items = set(row[1] for row in orders_data)
+        customer_orders = set(
+            row[1]
+            for row
+            in orders_data
+            if row[0] == customer
+        )
+
+        return possible_items - customer_orders
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        orders_data = self._data
+        possible_days = set(row[2] for row in orders_data)
+        days_by_customer = set(
+            row[2]
+            for row
+            in orders_data
+            if row[0] == customer
+        )
+
+        return possible_days - days_by_customer
 
     def get_busiest_day(self):
-        pass
+        orders_data = self._data
+        days_count = {}
+
+        for row in orders_data:
+            day = row[2]
+            days_count[day] = days_count.get(day, 0) + 1
+
+        return max(days_count, key=days_count.get)
 
     def get_least_busy_day(self):
-        pass
+        orders_data = self._data
+        days_count = {}
+
+        for row in orders_data:
+            day = row[2]
+            days_count[day] = days_count.get(day, 0) + 1
+
+        return min(days_count, key=days_count.get)
